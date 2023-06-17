83c891b24d983d8ba3414567d40857ca162c3985
HP <fatiqhussnain1@gmail.com>
2023-06-17 11:24:25 -0700

    added tab layout to view pager 2

--- app/src/main/java/com/example/tablayoutwithviewpager2/MainActivity.kt
+++ app/src/main/java/com/example/tablayoutwithviewpager2/MainActivity.kt
@@ -2,16 +2,21 @@ package com.example.tablayoutwithviewpager2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var viewPager: ViewPager2
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewPager = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
 
         val images = listOf(
             R.drawable.image1,
@@ -19,15 +24,31 @@ class MainActivity : AppCompatActivity() {
             R.drawable.image3
         )
 
-
         val adapter = ViewPagerAdapter(images)
         viewPager.adapter = adapter
 
-        viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
+//        viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
 
-        viewPager.beginFakeDrag()
-        viewPager.fakeDragBy(-10f)
-        viewPager.endFakeDrag()
+        // tabLayout jo ha wo google walo ki taraf sa hona chahiya ni to app ni chala gi in PagerWith TabLayout
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "Tab ${position + 1}"
+        }.attach()
 
+
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Selected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "UnSelected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "ReSelected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 }
